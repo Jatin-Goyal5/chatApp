14d13b7d08b8d0907803eea403dafda179bb2abf
Jatin-Goyal5 <jatinji145@gmail.com>
2022-03-10 22:42:04 +0530

    changes

--- public/script.js
+++ public/script.js
@@ -30,6 +30,23 @@ function openMenu() {
 }
 
 function showDialog(){
-    console.log('hello');
+    window.location = `truecallersdk://truesdk/web_verify?
+                               requestNonce=1234567510
+                               &partnerKey=IcHDNe44f3346f0c24e97b6bf27c9a4416330
+                               &partnerName=p2gchat
+                               &lang=English
+                               &title=logIn`;
+
+setTimeout(function() {
+
+  if( document.hasFocus() ){
+     // Truecaller app not present on the device and you redirect the user 
+     // to your alternate verification page
+  }else{
+     // Truecaller app present on the device and the profile overlay opens
+     // The user clicks on verify & you'll receive the user's access token to fetch the profile on your 
+     // callback URL - post which, you can refresh the session at your frontend and complete the user  verification
+  }
+}, 2000);
 }
 
